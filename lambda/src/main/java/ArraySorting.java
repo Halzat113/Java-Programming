4c920f5636de221bcf10dc2674a929c39ff39133
halzathalik <halzathalik@gmail.com>
2023-08-03 16:30:33 -0400

    Lambda intro

---
+++ lambda/src/main/java/ArraySorting.java
@@ -0,0 +1,26 @@
+public class ArraySorting {
+    public static void main(String[] args) {
+        ArraySorting as = new ArraySorting();
+        //as.sort();
+
+        QuickSort qs = new QuickSort();
+        BubbleSort bs = new BubbleSort();
+        as.sort(qs);
+        as.sort(bs);
+
+        Sorting noLambda = new QuickSort();
+        noLambda.sort();
+
+        Sorting quickSort = () -> System.out.println("Quick Sorting");
+        as.sort(quickSort);
+
+        Sorting bubbleSort = () -> System.out.println("Bubble Sorting");
+        as.sort(bubbleSort);
+
+    }
+
+    private void sort(Sorting sorting){
+       sorting.sort();
+    }
+}
+
---
+++ lambda/src/main/java/Sorting.java
@@ -0,0 +1,4 @@
+@FunctionalInterface
+public interface Sorting {
+    void sort();
+}
