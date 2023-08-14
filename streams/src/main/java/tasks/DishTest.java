436132bfd99c6be93e1708cb60c5415782036b21
halzathalik <halzathalik@gmail.com>
2023-08-14 19:48:11 -0400

    more stream tasks

--- streams/src/main/java/tasks/DishTest.java
+++ streams/src/main/java/tasks/DishTest.java
@@ -1,6 +1,7 @@
 package tasks;
 
 import java.util.Arrays;
+import java.util.Comparator;
 import java.util.List;
 
 public class DishTest {
@@ -17,9 +18,30 @@ public class DishTest {
                 new Dish("salmon",false,450,Type.FISH)
         );
         // print all dish's name that has less than 400 calories
+        System.out.println("==================================");
+
         menu.stream().filter(p->p.getCalories()<400).forEach(p-> System.out.println(p.getName()));
 
         //print the length of the name of each dish
+        System.out.println("==================================");
+
         menu.stream().map(f->f.getName()+" has "+f.getName().length()+" characters").forEach(System.out::println);
+
+        //print three high caloric dish Name(>300)
+        System.out.println("==================================");
+
+        menu.stream().filter(p->p.getCalories()>300).
+                sorted(Comparator
+                        .comparing(Dish::getCalories)
+                        .reversed())
+                .limit(3).map(Dish::getName)
+                .forEach(System.out::println);
+
+
+        //print all dish name that are below 400 calories in sorted
+        System.out.println("==================================");
+
+        menu.stream().filter(p->p.getCalories()<400).sorted(Comparator.comparing(Dish::getCalories)).map(Dish::getName)
+                .forEach(System.out::println);
     }
 }
