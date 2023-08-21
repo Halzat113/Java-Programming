ef4dffa617cfbcac5d614a9a20396c63c3177cff
halzathalik <halzathalik@gmail.com>
2023-08-21 15:42:04 -0400

    big decimal

--- streams/src/main/java/BigDecimalDemo.java
+++ streams/src/main/java/BigDecimalDemo.java
@@ -1,2 +1,29 @@
-package PACKAGE_NAME;public class BigDecimalDemo {
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+
+public class BigDecimalDemo {
+    public static void main(String[] args) {
+
+        double d1 = 174.56;
+        double d2 = 174.26;
+
+        System.out.println(d1-d2);
+
+        BigDecimal b1 = new BigDecimal(d1+"");
+        BigDecimal b2 = new BigDecimal(d2+"");
+
+        System.out.println(b1.subtract(b2));
+
+        //Scaling and Rounding
+        BigDecimal num1 = new BigDecimal(23.12);
+        System.out.println(num1.setScale(1, RoundingMode.CEILING));
+        System.out.println(num1.setScale(2, RoundingMode.CEILING));
+        System.out.println(num1.setScale(2, RoundingMode.FLOOR));
+
+        System.out.println(new BigDecimal("2").compareTo(new BigDecimal("2")));
+        System.out.println(new BigDecimal("2").compareTo(new BigDecimal("3")));
+
+        //BigDecimals are immutable
+
+    }
 }
